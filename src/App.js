abb7d36764ac84b6e7a10d809b5715a60cbec42b
kirbycampbell <jkirbycampbell@gmail.com>
2019-04-28 22:31:37 -0700

    Initial Set up for store-front

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="navbar">Vantage Vintage</div>
+      <img
+        className="imgager"
+        src="https://cdn.shopify.com/s/files/1/1132/5272/products/Dawn_a05aa900-3f5a-42d9-8170-0b6d07c4330a_2048x2048.jpg?v=1536795524"
+        alt="dress"
+      />
+      <img
+        className="imgager"
+        src="https://cdn.shopify.com/s/files/1/1132/5272/products/Sage_5536330b-009e-4941-9f5e-e385b9c781c9_2048x2048.jpg?v=1555432153"
+        alt="sagedress"
+      />
     </div>
   );
 }
